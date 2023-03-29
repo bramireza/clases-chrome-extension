2a7f5bdbb0829d80c67ac1c3ccbc3412297c3f0f
Brayian <bramirezag@gmail.com>
2023-03-28 19:44:08 -0500

    scrapt jobs

--- background.js
+++ background.js
@@ -1,8 +1,35 @@
+const saveObjectInLocalStorage = async function (obj) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.local.set(obj, function () {
+        resolve();
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+};
+
+const getObjectInLocalStorage = async function () {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.local.get(key, function (value) {
+        resolve(value);
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+};
 chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function ({ message }) {
-    if (message === "Hola BD")
-      port.postMessage({ message: "Te hablo desde el background popup" });
-    if (message === "Hola Background")
-      port.postMessage({ message: "Te hablo desde el background content" });
+  port.onMessage.addListener(async function ({ message }) {
+    if (message === "startScrap") {
+      const status = "start";
+      await saveObjectInLocalStorage(status);
+    }
+    if (message === "finish") {
+      /*const status = getObjectInLocalStorage("status");
+      if (status == "start")*/ port.postMessage({ message: "nextPage" });
+    }
   });
 });
--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,60 @@
 console.log("Ejecutando el content script 1.0");
 
-chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function ({ message }) {
-    alert(`${port.name}: message`);
-    if (message === "hola") port.postMessage({ message: "Como estas?" });
+const getJobInformation = () => {
+  const arrayJobs = [...document.querySelectorAll("[id*='jobcard-']")];
+  const jobs = arrayJobs.map((jobcard) => {
+    const [
+      { href: url },
+      {
+        children: [
+          {
+            children: [
+              { innerText: date },
+              { innerText: title },
+              { innerText: salaryRange },
+              { innerText: benefits },
+              { innerText: description },
+              companyCity,
+            ],
+          },
+        ],
+      },
+    ] = jobcard.children;
+
+    const company = companyCity?.querySelector("label")?.innerText;
+    const city = companyCity?.querySelector("p")?.innerText;
+
+    return {
+      date: date.split("\n")[0],
+      title,
+      salaryRange,
+      benefits: benefits.split("\n"),
+      description,
+      company,
+      city,
+      url,
+    };
   });
-});
+  return jobs;
+};
 
 //Connect to background
-const port = chrome.runtime.connect({ name: "content-background" });
-port.postMessage({ message: "Hola Background" });
-port.onMessage.addListener(async ({ message }) => {
-  alert(message);
+const portBackground = chrome.runtime.connect({ name: "content-background" });
+
+portBackground.onMessage.addListener(async ({ message }) => {
+  if (message === "nextPage") {
+    const nextPageButton = document.querySelector("[class*=next-]");
+    nextPageButton.click();
+  }
+});
+
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function ({ message }) {
+    //alert(`${port.name}: message`);
+    if (message === "getJobs") {
+      const jobs = getJobInformation();
+      port.postMessage({ message: "ok", data: jobs });
+      portBackground.postMessage({ message: "finish" });
+    }
+  });
 });
